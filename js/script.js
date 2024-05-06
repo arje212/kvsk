ae7ad3eda859f73c50f9ad3cd3a50c40fa37b86c
Rolando Melecio <114086629+arje212@users.noreply.github.com>
2024-05-06 15:52:04 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,96 @@
+let Navbar = document.querySelector('.navbar');
+let Fabars = document.querySelector('.fa-bars');
+
+Fabars.onclick = () =>{
+    Navbar.classList.toggle("active")
+};
+
+
+
+
+var swiper = new Swiper(".home-slid", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+
+var swiper = new Swiper(".team-slider", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      "0": {
+        slidesPerView: 1,
+        autoplay:{
+            delay:700,
+            disableOnInteraction:false,
+        },
+      },
+      "768": {
+        slidesPerView: 2,
+          
+      },
+      "1020": {
+        slidesPerView: 3,
+          
+      },
+    },
+  });
+/**image */
+
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
